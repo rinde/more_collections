131c061002ef70fab6b4c503dd1e4d57b9518f44
Rinde van Lon <rindevanlon@gmail.com>
2021-12-27 17:28:53 +0000

    add header comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
+//! More collection types.
+//!
+//! # Multimap
+//!
+//! A collection that maps keys to values, similar to
+//! [HashMap](std::collections::HashMap), but where each key may be associated
+//! with _multiple_ values. Multimaps can be visualized as a map from keys to
+//! non-empty collections of values:
+//! - a → 0, 1
+//! - b → 2
+//!
+//! Or, it can be visualized as a collection of key-value pairs:
+//! - a → 0
+//! - a → 1
+//! - b → 2
+//!
+//! The multimap API is based on the second form, `len() == 3` and `keys_len()
+//! == 2` for the above example.
+//!
+//! | Name               | Behaves as |
+//! | ------------------ | ----- |
+//! | [HashSetMultimap]  | [HashMap](std::collections::HashMap)`<K,
+//! `[HashSet](std::collections::HashSet)`<V>>` | [HashVecMultimap]  |
+//! [HashMap](std::collections::HashMap)`<K, `[Vec](std::vec::Vec)`<V>>`
+//! | [IndexSetMultimap] | [IndexMap](indexmap::IndexMap)`<K,
+//! `[HashSet](indexmap::IndexSet)`<V>>` | [IndexVecMultimap] |
+//! [IndexMap](indexmap::IndexMap)`<K, `[Vec](std::vec::Vec)`<V>>`
+
 mod multimap;
 
 pub use multimap::*;
